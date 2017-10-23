03cc445616d008cd05be41a55e6855ebdced7187
Ryan Huang <ryan_huang@intuit.com>
2017-10-23 13:39:22 -0700

    added src

---
+++ src/components/Channel/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import Video from 'react-native-video';
+
+class Channel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handlePress = this.handlePress.bind(this);
+    }
+
+    handlePress() {
+        this.props.onPress(this.props.channelURL);
+    }
+
+    render() {
+        const imageURL = this.props.imageURL.split("'")[1];
+
+        return (
+            <TouchableOpacity
+                onPress={this.handlePress}
+                style={styles.container}
+            >
+                <View>
+                    <Image
+                        style={{ width: 150, height: 150 }}
+                        source={{ uri: imageURL }}
+                    />
+                    <Text>{this.props.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#F8F8F8'
+  },
+  video: {
+    width: 50,
+    height: 50
+  }
+});
+
+export default Channel;
---
+++ src/components/ChannelList/index.js
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import axios from 'axios';
+import { parseString } from 'xml2js';
+
+import Header from '../header/index';
+import Channel from '../Channel/index';
+import Show from '../Show/index';
+import Episode from '../Episode/index';
+
+class ChannelList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            channels: null,
+            shows: null,
+            episodes: null,
+            currentList: null,
+            loading: false,
+            refreshing: false
+        };
+
+        this.rssLink = 'https://irss.se/dramas';
+        this.handleChannelPress = this.handleChannelPress.bind(this);
+        this.handleShowPress = this.handleShowPress.bind(this);
+        this.handleEpisodePress = this.handleEpisodePress.bind(this);
+    }
+
+    componentWillMount() {
+        this.fetchChannel(this.rssLink);
+    }
+
+    fetchChannel(url) {
+        axios.get(url)
+            .then(response => {
+                parseString(response.data, (error, result) => {
+                    console.log(result.rss.channel[0].item);
+                    this.setState({
+                        channels: result.rss.channel[0].item,
+                        currentList: 'channels'
+                    });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    fetchShows(url) {
+        axios.get(url)
+            .then(response => {
+                parseString(response.data, (error, result) => {
+                    console.log(result.rss.channel[0].item);
+                    this.setState({
+                        shows: result.rss.channel[0].item,
+                        currentList: 'shows'
+                    });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    fetchEpisodes(url) {
+        axios.get(url)
+            .then(response => {
+                parseString(response.data, (error, result) => {
+                    console.log(result.rss.channel[0].item);
+                    this.setState({
+                        episodes: result.rss.channel[0].item,
+                        currentList: 'episodes'
+                    });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    imageURLParser(imageURL) {
+        return imageURL.split("'")[1];
+    }
+
+    handleChannelPress(url) {
+        console.log(url);
+        this.fetchShows(url);
+    }
+
+    handleShowPress(url) {
+        console.log(url);
+        this.fetchEpisodes(url);
+    }
+
+    handleEpisodePress(url) {
+        console.log(url);
+    }
+
+    renderChannel = ({ item }) => (
+        <Channel
+            key={item.title[0]}
+            title={item.title[0]}
+            bookmark={item.bookmark[0]}
+            imageURL={item.description[0]}
+            channelURL={item.enclosure[0].$.url}
+            onPress={this.handleChannelPress}
+        />
+    )
+
+    renderShow = ({ item }) => (
+        <Show
+            key={item.title}
+            title={item.title}
+            bookmark={item.bookmark}
+            imageURL={item.description[0]}
+            channelURL={item.enclosure[0].$.url}
+            onPress={this.handleShowPress}
+        />
+    )
+
+    renderEpisode = ({ item }) => (
+        <Episode
+            key={item.title}
+            title={item.title}
+            bookmark={item.bookmark}
+            imageURL={item.description[0]}
+            channelURL={item.enclosure.url}
+            onPress={this.handleEpisodePress}
+        />
+    )
+
+    renderList() {
+        switch (this.state.currentList) {
+            case 'channels':
+                return (
+                    <List>
+                        <FlatList
+                            data={this.state.channels}
+                            keyExtractor={item => item.title[0]}
+                            renderItem={this.renderChannel}
+                            numColumns={2}
+                        />
+                    </List>
+                );
+            case 'shows':
+                return (
+                    <List>
+                        <FlatList
+                            data={this.state.shows}
+                            keyExtractor={item => item.title}
+                            renderItem={this.renderShow}
+                            numColumns={2}
+                        />
+                    </List>
+                );
+            case 'episodes':
+                return (
+                    <List>
+                        <FlatList
+                            data={this.state.episodes}
+                            keyExtractor={item => item.title}
+                            renderItem={this.renderEpisode}
+                            numColumns={2}
+                        />
+                    </List>
+                );
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                { this.renderList() }
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink'
+    }
+});
+
+export default ChannelList;
